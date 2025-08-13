558959367e42a603c2c7a069a64a1201c49cc6ba
jhui-cai <2287505277@qq.com>
2025-08-13 09:30:22 +0000

    Deploying to gh-pages from @ radiolab-gzhu/radiolab-gzhu.github.io@3e7629ff3522e8fe02a644e56e1c13fb37ef8fda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-members",
-          title: "members",
-          description: "members of our radio lab",
+          title: "Members",
+          description: "Members of our Radio Lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/members";
